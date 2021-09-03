4dc9a19184d9c46b587bd0e69dd47e43fdf9b480
Aditya Singh <adityakrsingh1999@gmail.com>
2021-09-03 19:10:15 +0530

    Sliding window

--- LeetCode/Medium/SlidingWindow.java
+++ LeetCode/Medium/SlidingWindow.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SlidingWindow {
 	
@@ -49,15 +50,74 @@ public class SlidingWindow {
 	}
 	
 	
+	
+	//COunt occurence
+	void countAnagrams(String s1, String s2) {
+			ArrayList<Integer> list= new ArrayList<>();
+			HashMap<Character, Integer> map= new HashMap<>();
+			//We map all the charcater of target string
+			for(char c:s2.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
+			
+			int counter= map.size();
+			int start=0, end=0;
+			
+			while(end < s1.length()) {
+				char c= s1.charAt(end);
+				if(map.containsKey(c)) {
+					map.put(c, map.get(c)-1 );
+					if(map.get(c) == 0)
+						counter--;
+				}
+				end++;
+				
+				//if counter ==0 that means we find an anagram 
+				while(counter == 0) {
+					char ch= s1.charAt(start);
+					if(map.containsKey(ch)) {
+						map.put(ch, map.getOrDefault(ch, 0) + 1);
+						if(map.get(ch) > 0) 
+							counter++;
+					}
+					
+					if(end - start == s2.length()) {
+						list.add(start);
+					}
+					start++;
+					
+				}
+				
+				
+			}
+			System.out.println(list);
+			
+			
+		 	
+	        
+	}
+	
+	
+	
 	public static void main(String[] args) {
+		
+		SlidingWindow obj= new SlidingWindow();
+		
+		//Find all max subarray with size 
 		int nums[]= {4,7,1,3,5,2,9,1,5};
 		int size=3;
 		
+		//find all first Negetives in an array
 		int negs[]= {12,-1,3,-3,9,5,-7,11,-4,2,-8,-9,-1,12,4,13};
-//		int size=3;
-		SlidingWindow obj= new SlidingWindow();
+//		obj.firstNegetive(negs, size);
+		
+		
+		//Count occurences of anagrams
+		String s1="abcjhjfbcaklsdjljnscablskjdacb";
+		String s2="abc";
+		obj.countAnagrams(s1, s2);
+		
+		
 //		System.out.println(obj.findMaxSubArray(nums, size));
-		obj.firstNegetive(negs, size);
+		
 	}
 
 }
