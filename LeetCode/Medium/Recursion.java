e8550e026457badf373494a8527b87b84eb30f19
Aditya Singh <adityakrsingh1999@gmail.com>
2021-09-05 19:46:02 +0530

    Recursion

--- LeetCode/Medium/Recursion.java
+++ LeetCode/Medium/Recursion.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 
 public class Recursion {
 	void printN(int n) {
@@ -31,13 +32,42 @@ public class Recursion {
 	}
 	
 	
+	
+	// Sort a stack
+	
+//	private void sortStack(Stack<Integer> st) {
+//		if(st.isEmpty()) return;
+//		sortStack(st.pop());
+//		insert
+//		printStack(st);
+//	}
+	
+	
+	
+	private void builtStack(Stack<Integer>st, int nums[]) {
+		for(int i:nums) st.push(i);
+	}
+	
+	private void printStack(Stack<Integer> st) {
+		while(!st.isEmpty()) System.out.print(st.pop()+" ");
+	}
+	
+
+	
+	
 	public static void main(String[] args) {
 		Recursion obj= new Recursion();
 //		obj.printN(20);
 		
 		int nums[]= {9,4,1,3,5,2,7,6,8,10};
 		obj.sort(nums, nums.length);
-		for(int i:nums) System.out.print(i+" ");
+//		for(int i:nums) System.out.print(i+" ");
+		
+		//Sort a stack
+		Stack<Integer> st= new Stack<>();
+		obj.builtStack(st, nums);
+		obj.printStack(st);
+		
 	}
 
 }
