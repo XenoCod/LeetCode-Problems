80df4e0877b0f28a243328ceab548496f2b0d997
Aditya Singh <adityakrsingh1999@gmail.com>
2021-09-07 19:18:15 +0530

    Binary Search

--- LeetCode/Medium/BinarySearch.java
+++ LeetCode/Medium/BinarySearch.java
@@ -129,11 +129,36 @@ public class BinarySearch {
 		
 	}
 	
+	
+	
+	//Search in sorted rotated array with duplicates
+	private boolean searchSortedRotatedArray2(int nums[], int target) {
+		int low=0, high=nums.length-1;
+        while(low <= high){
+            int mid= low + (high - low) /2;
+            if(nums[mid] ==  target) return true;
+            if(nums[low] <= nums[mid]){
+                if(nums[low]<= target && target<= nums[mid])  high= mid-1;
+                else                                          low=mid+1;
+                
+            }
+            else{
+                if(nums[mid]<= target && target<= nums[high]) low= mid+1;
+                else                                          high=mid-1;
+            }
+        }
+        return false;
+       
+	}
+	
 	public static void main(String[] args) {
 		int nums[]= {2,4,10,10,10,18,20};
 		int infiniteNums[]= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 		int absDiff[]= {1,3,8,10,15};
 		int bio[]= {1,3,8,12,4,2};
+		int dups[]= {2,5,6,0,0,1,2};
+		
+		
 		
 		BinarySearch bs= new BinarySearch();
 		
@@ -144,6 +169,7 @@ public class BinarySearch {
 		System.out.println(bs.firstOcc(infiniteNums,2));
 		System.out.println(bs.absDif(absDiff, 12));
 		System.out.println(bs.biotonicSearch(bio, 2 ));
+		System.out.println(bs.searchSortedRotatedArray2(dups, 0));
 	}
 
 }
